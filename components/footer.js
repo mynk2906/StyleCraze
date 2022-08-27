80fd12bf12f2acadc6c50490e4d235931ce1ef2d
mynk2906 <msaxena2906@gmail.com>
2022-08-27 21:28:04 +0530

    landing page completed

---
+++ components/footer.js
@@ -0,0 +1,61 @@
+let footer = ()=>{
+    return`
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-11 m-auto">
+                <div class="">
+                    <div class="row bg-white p-5 rounded-3 text-center footerUl">
+                      <div class="col-md-3 mb-3">
+                        <ul class="text-md-start text-center">
+                          <li>About us</li>
+                          <li>Images Usage Policy</li>
+                          <li>Contact US</li>
+                          <li>Editorial Guidelines</li>
+                          <li>Join Our Medical Board</li>
+                          <li>Press Room</li>
+                        </ul>
+                      </div>
+                      <div class="col-md-3 mb-3">
+                        <ul class="text-md-start text-center">
+                          <li> Advertise With Us</li>
+                          <li> Privacy Policy</li>
+                          <li> Terms Of Use</li>
+                          <li> Affiliate Disclosure</li>
+                          <li> Cookie Policy</li>
+                        </ul>
+                      </div>
+                      <div class="col-md-3 mb-3 text-center">
+                       <p class="fs-4">Follow us</p>
+                       <ul class="footer-logo-section social-media-icon">
+                        <li class=""><img src="https://www.stylecraze.com/wp-content/themes/buddyboss-child/images/svg-image/facebook-f.svg" width="15" alt=""></li>
+                        <li class=""><img src="https://www.stylecraze.com/wp-content/themes/buddyboss-child/images/svg-image/pinterest-p.svg" width="15" alt=""></li>
+                        <li class=""><img src="https://www.stylecraze.com/wp-content/themes/buddyboss-child/images/svg-image/twitter.svg" width="15" alt=""></li>
+                        <li class=""><img src="https://www.stylecraze.com/wp-content/themes/buddyboss-child/images/svg-image/instagram.svg" width="15" alt=""></li>
+                        <li class=""><img src="https://www.stylecraze.com/wp-content/themes/buddyboss-child/images/svg-image/youtube.svg" width="15" alt=""></li>
+                      </ul>
+                      </div>
+                      <div class="col-md-3 mb-3">
+                        <ul class="text-md-start text-center">
+                          <li>Our Sister Sites</li>
+                          <li>MomJunction</li>
+                          <li>The BridalBox</li>
+                          <li>Skinkraft</li>
+                          <li>Vedix</li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center copyright-bkg-clr pb-2">
+      <div class="w-75 m-auto">
+        <p class="m-1 fs-5">Copyright © 2011 - 2022 <strong class="website-color">IncNut Stylecraze Private Limited.</strong>All rights reserved.</p>
+        <p class="m-1">StyleCraze provides content of general nature that is designed for informational purposes only. The content is not intended to be a substitute</p>
+        <p class="">for professional medical advice, diagnosis, or treatment. <span class="website-color">Click here for additional information.</span></p>
+      </div>
+    </div>`
+}
+
+export default footer;
---
+++ components/navbar.js
@@ -0,0 +1,59 @@
+let navbar = (images) =>{
+    return`
+        <div class="row" >
+            <div class="col-12" >
+                <div class="row">
+                    <div class="col-12 navbar-lower-section navbar-hover-top-section" >
+                        <ul>
+                            <li id="one">Bridal Makeup</li>
+                            <li id="two">Celebraty Makeup</li>
+                            <li id="three">Eye Make up</li>
+                            <li id="four">Face Makeup</li>
+                            <li id="five">Lip Make up</li>
+                            <li id="six">Makeup Ideas</li>
+                            <li id="seven">Mehandi Design</li>
+                            <li id="eight">Nail Art</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 navbar-second-part">
+                        <ul>
+                            <li>
+                                <img src="${images[0]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                            <li>
+                                <img src="${images[1]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                            <li>
+                                <img src="${images[2]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                            <li>
+                                <img src="${images[3]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                            <li>
+                                <img src="${images[4]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                            <li>
+                                <img src="${images[5]}" alt="">
+                                <p>100 Most Beautiful Indian Bridal Makeup Looks You’ll Love</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>`
+}
+
+let fun = (cond) =>{
+    document.getElementById(cond).style.backgroundColor="white";
+}
+
+export  {navbar,fun};   

92ee53a636a37b860f4febb894bbb83b31fedcd8
Ravi515K <koliravi95@gmail.com>
2022-08-27 22:20:38 +0530

    navbarpage

---
+++ Health/navbar.js
@@ -0,0 +1,157 @@
+
+function navbar(){
+    return `
+    <div id="navbar">
+        
+    <ul>
+     <div id="line">
+         <hr>
+         <hr>
+         <hr>
+     </div>
+     
+     
+ 
+      <li><a href="">MAKEUP <span>|</span>  </a>
+      
+         <div class="drop">
+             <ul>
+                 <li><a href="">BRIDAL MAKEUP</a></li>
+                 <li><a href="">CELEBRITY MAKEUP</a></li>
+                 <li><a href="">EYE MAKEUP</a></li>
+                 <li><a href="">LIP MAKEUP</a></li>
+                 <li><a href="">MAKEUP IDEAS</a></li>
+                 <li><a href="">NAIL ART</a></li>
+                 <li><a href="">MEHANDI DESIGNS</a></li>
+             </ul>
+         </div>
+   
+      </li>
+
+      <li><a href="">HAIR <span>|</span> </a>
+        
+         <div class="drop">
+             <ul>
+                 <li><a href="">BASIC HAIR CARE</a></li>
+                 <li><a href="">DANDRUFF</a></li>
+                 <li><a href="">HAIR CARE IDEAS</a></li>
+                 <li><a href="">DRY HAIR CARE</a></li>
+                 <li><a href="">HAIR COLOR </a></li>
+                 <li><a href="">HAIR FALL</a></li>
+                 <li><a href="">HAIR GROWTH</a></li>
+                 <li><a href="">HAIR TREATMENT</a></li>
+                 <li><a href="">OILY HAIR CARE</a></li>
+             </ul>
+         </div>
+     
+      </li>
+
+      <li><a href="">SKIN CARE <span>|</span> </a>
+         
+         <div class="drop">
+             <ul>
+                 <li><a href="">ACNE</a></li>
+                 <li><a href="">ANTI AGEING</a></li>
+                 <li><a href="">BEAUTY SECRETS</a></li>
+                 <li><a href="">DRY SKIN</a></li>
+                 <li><a href="">FACE CARE TIPS</a></li>
+                 <li><a href="">FACE PACK MASK</a></li>
+                 <li><a href="">GLOWING SKIN</a></li>
+                 <li><a href="">SKIN CARE IDEAS</a></li>
+                 <li><a href="">OILY SKIN</a></li>
+                 <li><a href="">PERFUMES</a></li>
+                 <li><a href=""> SKIN CARE PROBLEM</a></li>
+                 <li><a href="">SUNSCREEN</a></li>
+             </ul>
+         </div>
+     
+      </li>
+     
+      <li><a href="">HAIRSTYLES <span>|</span> </a>
+     
+         <div class="drop">
+             <ul>
+                 <li><a href="">BOB HAIRSTYLE</a></li>
+                 <li><a href="">BRIDE HAIRSTYLE</a></li>
+                 <li><a href="">BRIDAL HAIRSTYLE</a></li>
+                 <li><a href="">BUN HAIRSTYLE</a></li>
+                 <li><a href="">CELEBRITY HAIRSTYLE</a></li>
+                 <li><a href="">CURLY HAIRSTYLE</a></li>
+                 <li><a href="">DIFFERENT HAIRSTYLE</a></li>
+                 <li><a href="">HAIRSTYLE TRENDS</a></li>
+                 <li><a href="">MEDIUM HAIRSTYLE</a></li>
+                 <li><a href="">LONG HAIRSTYLE</a></li>
+                 <li><a href=""> SHORT HAIRSTYLE</a></li>
+                 <li><a href="">UPDO HAIRSTYLE</a></li>
+               
+     
+             </ul>
+         </div>
+     
+      </li>
+
+      <li><a href="">HEALTH & WELLNESS  <span>|</span> </a>
+   
+         <div class="drop">
+             <ul>
+                 <li><a href="">DIET TIPS</a></li>
+                 <li><a href="">HEALTH DEVICES</a></li>
+                 <li><a href="">HEALTHY FOODS</a></li>
+                 <li><a href="">HOME REMEDIES</a></li>
+                 <li><a href="">NUTRITION </a></li>
+                 <li><a href="">FITNESS</a></li>
+                 <li><a href="">WEIGHT LOSS</a></li>
+                 <li><a href="">WEIGHT GAIN </a></li>
+                 <li><a href="">YOGA</a></li>
+                
+             </ul>
+         </div>
+     
+      </li>
+
+      <li><a href="">NEWS <span>|</span> </a>
+  
+         <div class="drop">
+             <ul>
+                 <li><a href="">COLLABORATION </a></li>
+                 <li><a href="">BRAND NEWS</a></li>
+                 <li><a href="">FASHION</a></li>
+                 <li><a href="">CELEBRITY NEWS</a></li>
+                 <li><a href="">INSPIRATION</a></li>
+                 <li><a href="">TOP 10'S</a></li>
+                 <li><a href="">TRENDS</a></li>
+                 <li><a href="">WOMEN EMPOWERMENTS</a></li>
+                 <li><a href="">RELATIONSHIP</a></li>
+                 
+             </ul>
+         </div>
+     
+      </li>
+
+      <li><a href="">TOOLS <span>|</span> </a></li>
+     
+      <li><a href="">PRODUCTS <span>|</span></a></li>
+   
+      <li><a href="">ABOUT US  </a>
+     
+         <div class="drop">
+             <ul>
+                 
+                 <li><a href="">OUR TEAM</a></li>
+                 <li><a href="">EDITORIAL POLICY</a></li>
+                 <li><a href="">MEDICAL REVIEW BOARD</a></li>
+                 <li><a href="">EXPERT PANNEL</a></li>
+                 <li><a href="">CONATACT US</a></li>
+                 
+             </ul>
+         </div>
+     
+      </li>
+
+
+    
+     </ul>
+ </div>
+    `
+}
+ export default navbar;
